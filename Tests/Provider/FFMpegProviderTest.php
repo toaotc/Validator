83f0f0433210e1c3887c803a0603c26afc7f14d8
Enrico Thies <enrico.thies@gmail.com>
2013-10-13 17:03:03 +0200

    removed FFMpeg mocking

--- Tests/Provider/FFMpegProviderTest.php
+++ Tests/Provider/FFMpegProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace Toa\Component\Validator\Tests\Provider;
 
+use FFMpeg\FFMpeg;
 use Toa\Component\Validator\Provider\FFMpegProvider;
 
 /**
@@ -20,43 +21,7 @@ class FFMpegProviderTest extends \PHPUnit_Framework_TestCase
             $this->markTestSkipped('The "FFMpeg" component is not available');
         }
 
-        $streamMock = $this->getMockForAbstractClass(
-            'FFMpeg\FFProbe\DataMapping\AbstractData',
-            array(
-                array(
-                    'duration' => 11.516667,
-                    'height' => 240,
-                    'width' => 480,
-                )
-            )
-        );
-
-        $streamCollectionMock = $this->getMock('FFMpeg\FFProbe\DataMapping\StreamCollection');
-        $streamCollectionMock
-            ->expects($this->any())
-            ->method('first')
-            ->will($this->returnValue($streamMock));
-
-        $abstractStreamableMediaMock = $this->getMock(
-            'FFMpeg\Media\AbstractStreamableMedia',
-            array(),
-            array(),
-            '',
-            false
-        );
-        $abstractStreamableMediaMock
-            ->expects($this->any())
-            ->method('getStreams')
-            ->will($this->returnValue($streamCollectionMock));
-
-        $ffmpegMock = $this->getMock('FFMpeg\FFMpeg', array(), array(), '', false);
-        $ffmpegMock
-            ->expects($this->any())
-            ->method('open')
-            ->will($this->returnValue($abstractStreamableMediaMock));
-
-
-        $this->provider = new FFMpegProvider($ffmpegMock);
+        $this->provider = new FFMpegProvider(FFMpeg::create());
 
         $this->video = __DIR__.'/../Constraints/Fixtures/white.m4v';
     }
